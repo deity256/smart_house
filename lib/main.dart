9bb36c0db82cce83c552db968bbf67bbffed1f9a
Ebrahim Soliman <esoliman@nbsventure.com>
2020-11-13 19:55:55 +0200

    remove route

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,20 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:smart_house/colors.dart';
-import 'package:smart_house/router/route_guards.dart';
-import 'package:smart_house/router/router.gr.dart';
 
 import 'air_conditioner_view.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // builder uses the native nav key to keep
-      // the state of ExtendedNavigator so it won't reload
-      // when using Flutter tools-> select widget mode
-      builder: ExtendedNavigator.builder<AppRouter>(
-          router: AppRouter(),
-          initialRoute: '/',
-          guards: [AuthGuard()],
-          builder: (ctx, extendedNav) => Theme(
-                data: ThemeData.dark(),
-                child: extendedNav,
-              )),
-    );
-  }
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: AppColors.transparentColor,
+      statusBarIconBrightness: Brightness.dark));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Smart House',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    ),
+    home: AirConditionerView(),
+  ));
 }
